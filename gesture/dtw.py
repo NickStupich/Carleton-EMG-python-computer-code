f9ac29abaf52e2a303a6cf4a58aa405ba4701427
Nick Stupich <nick.stupich@gmail.com>
2012-02-29 01:42:43 -0500

    speed up my own dtw variant slightly, fixed the basic one so it works as a basic dtw algorithm should

--- gesture/dtw.py
+++ gesture/dtw.py
@@ -8,36 +8,27 @@ def dtwBasic(lx, ly, distanceFunc):
 	lenX = len(lx)
 	lenY = len(ly)
 	
-	costs = [[None for y in range(lenY)] for x in range(lenX)]
-	
-	for x in range(lenX):
-		costs[x][0] = INFINITY
-	for y in range(lenY):
-		costs[0][y] = INFINITY
-		
-	costs[0][0] = distanceFunc(lx[0], ly[0])
-	
-	for x in range(1, lenX):
-		for y in range(1, lenY):
-			cost = distanceFunc(lx[x], ly[y])
-			costs[x][y] = cost
-	
 	results = [[None for y in range(lenY)] for x in range(lenX)]
 	
+	for x in range(lenX):
+		results[x][0] = INFINITY
+	for y in range(lenY):
+		results[0][y] = INFINITY
+		
+	results[0][0] = 0
+	
 	for x in range(1, lenX):
 		for y in range(1, lenY):
-			results[x][y] = costs[x][y] + min(	costs[x-1][y],
-												costs[x][y-1],
-												costs[x-1][y-1])
+			results[x][y] = distanceFunc(lx[x], ly[y])  + min(	results[x-1][y],
+																results[x][y-1],
+																results[x-1][y-1])
 	
-	printCostMatrix(costs)
-	print ' '
 	printCostMatrix(results)
 	print ' '
 	return results[-1][-1]
 		
 """
-HOW THIS MONSTROSITY WORKS:
+HOW THIS MONSTROSITY (OF AWESOMENESS) WORKS:
 1 - understand regular dtw:
 	stretch a set of discrete points in time(in this case), such that they fit best together
 2 - one signal(or both) may have extra data on either end that isn't relevant to the gesture that is meant to be identified
@@ -55,25 +46,15 @@ def dtwAcceptPortionOfInputs(lx, ly, distanceFunc):
 	lenX = len(lx)
 	lenY = len(ly)
 	
-	costs = [[None for y in range(lenY)] for x in range(lenX)]
+	costs = [[distanceFunc(lx[x], ly[y]) for y in range(lenY)] for x in range(lenX)]
 	
 	for x in range(lenX):
-		costs[x][0] = IGNORING_PENALTY_FUNC(float(x) / lenX) + distanceFunc(lx[x], ly[0])
-	for y in range(lenY):
-		costs[0][y] = IGNORING_PENALTY_FUNC(float(y) / lenY) + distanceFunc(lx[0], ly[y])
-		
-	costs[0][0] = distanceFunc(lx[0], ly[0])
-	
-	for x in range(1, lenX):
-		for y in range(1, lenY):
-			costs[x][y] = distanceFunc(lx[x], ly[y])
-			
-	#add the ignoring penalty on the 'exit', last row/column
-	for x in range(lenX):
+		costs[x][0] += IGNORING_PENALTY_FUNC(float(x) / lenX)
 		costs[x][-1] += IGNORING_PENALTY_FUNC((lenX - float(x)-1) / lenX)
 	for y in range(lenY):
+		costs[0][y] += IGNORING_PENALTY_FUNC(float(y) / lenY)
 		costs[-1][y] += IGNORING_PENALTY_FUNC((lenY - float(y)-1) / lenY)
-							
+		
 	#initializing with costs means that the top\left row\column work out
 	results = [[costs[x][y] for y in range(lenY)] for x in range(lenX)]
 	
--- gesture/gestureRecognizer1.py
+++ gesture/gestureRecognizer1.py
@@ -2,6 +2,17 @@ import stats
 import math
 import dtw
 import distances
+from datetime import datetime
+
+def speedTest():
+	dFunc = distances.euclideanDistance
+	lx = [[math.sin(x/10.0)] for x in range(10)]
+	ly = [[math.sin(x/10.0 + 0.5)] for x in range(10)]
+	start = datetime.now()
+	for _ in range(1000):
+		dist = dtw.dtwAcceptPortionOfInputs(lx, ly, dFunc)
+	end = datetime.now()
+	print (end - start)
 
 def test():
 	dFunc = distances.euclideanDistance
@@ -10,8 +21,8 @@ def test():
 	#print lx, '\n'
 	#print ly, '\n'
 	
-	lx = [[x] for x in [0, 0, 0, 1, 2, 3, 3, 3, 2]]
-	ly = [[x] for x in [1, 2, 3, 3, 3, 2]]
+	lx = [[float(x)] for x in [0, 0, 0, 1, 2, 2, 3, 3, 3, 2]]
+	ly = [[float(x)] for x in [1, 2, 3, 3, 3, 2, 0, 1]]
 	
 	#print dFunc(lx[-1], ly[6])
 	
@@ -20,9 +31,14 @@ def test():
 	
 	#print distances.euclideanDistance([1.0], [5.0])
 	
-	#dist = dtw.dtwBasic(lx, ly, distances.euclideanDistance)
+	dist = dtw.dtwBasic(lx, ly, distances.euclideanDistance)
+	print dist
+	#speedTest()
 	dist = dtw.dtwAcceptPortionOfInputs(lx, ly, dFunc)
 	print dist
+	
+#def train():
+	
 	
 if __name__ == "__main__":
 	test()
