0d193c853648c209ee27407cc872d042e2890a06
Nick Stupich <nick.stupich@gmail.com>
2012-02-29 01:19:45 -0500

    dtw with removing of times at beginning/end appears to be working, probably not optimal though

--- gesture/dtw.py
+++ gesture/dtw.py
@@ -2,7 +2,7 @@
 INFINITY = float('inf')
 
 def printCostMatrix(costs):
-	print '\n'.join('\t'.join(str(x)[:4] for x in row) for row in costs)
+	print '\n'.join('\t'.join(str(round(x, 3)) for x in row) for row in costs)
 
 def dtwBasic(lx, ly, distanceFunc):
 	lenX = len(lx)
@@ -29,17 +29,28 @@ def dtwBasic(lx, ly, distanceFunc):
 			results[x][y] = costs[x][y] + min(	costs[x-1][y],
 												costs[x][y-1],
 												costs[x-1][y-1])
-									
+	
 	printCostMatrix(costs)
 	print ' '
 	printCostMatrix(results)
 	print ' '
 	return results[-1][-1]
 		
+"""
+HOW THIS MONSTROSITY WORKS:
+1 - understand regular dtw:
+	stretch a set of discrete points in time(in this case), such that they fit best together
+2 - one signal(or both) may have extra data on either end that isn't relevant to the gesture that is meant to be identified
+	- so allow the algorithm to creep into the beginning of either sample, at a cost proportional to the square of the percent of the way it creeps in
+	- the algorithm can also creep into the end of either sample, at the same cost
+	- the algorithm picks the cost of the best path through, starting from anywhere and ending anywhere (anywhere in both samples being compared), and returns this cost
+"""
+
 #we can't just let the algorithm ignore stuff without a penalty, so create a function to 
 #assign values to the outer row/column of the matrix instead of using infinity
 #this way the algorithm should carry along that spot for a bit before it has to jump into the real thing
-IGNORING_PENALTY_FUNC = lambda x: x * 1.0
+IGNORING_PENALTY_FUNC = lambda r: r * r * 2.0
+
 def dtwAcceptPortionOfInputs(lx, ly, distanceFunc):
 	lenX = len(lx)
 	lenY = len(ly)
@@ -47,27 +58,38 @@ def dtwAcceptPortionOfInputs(lx, ly, distanceFunc):
 	costs = [[None for y in range(lenY)] for x in range(lenX)]
 	
 	for x in range(lenX):
-		costs[x][0] = IGNORING_PENALTY_FUNC(float(x) / lenX)
+		costs[x][0] = IGNORING_PENALTY_FUNC(float(x) / lenX) + distanceFunc(lx[x], ly[0])
 	for y in range(lenY):
-		costs[0][y] = IGNORING_PENALTY_FUNC(float(y) / lenY)
+		costs[0][y] = IGNORING_PENALTY_FUNC(float(y) / lenY) + distanceFunc(lx[0], ly[y])
 		
 	costs[0][0] = distanceFunc(lx[0], ly[0])
 	
 	for x in range(1, lenX):
 		for y in range(1, lenY):
-			cost = distanceFunc(lx[x], ly[y])
-			costs[x][y] = cost
-	
-	results = [[None for y in range(lenY)] for x in range(lenX)]
+			costs[x][y] = distanceFunc(lx[x], ly[y])
+			
+	#add the ignoring penalty on the 'exit', last row/column
+	for x in range(lenX):
+		costs[x][-1] += IGNORING_PENALTY_FUNC((lenX - float(x)-1) / lenX)
+	for y in range(lenY):
+		costs[-1][y] += IGNORING_PENALTY_FUNC((lenY - float(y)-1) / lenY)
+							
+	#initializing with costs means that the top\left row\column work out
+	results = [[costs[x][y] for y in range(lenY)] for x in range(lenX)]
 	
 	for x in range(1, lenX):
 		for y in range(1, lenY):
-			results[x][y] = costs[x][y] + min(	costs[x-1][y],
-												costs[x][y-1],
-												costs[x-1][y-1])
-									
-	printCostMatrix(costs)
-	print ' '
-	printCostMatrix(results)
-	print ' '
-	return results[-1][-1]
+			results[x][y] = costs[x][y] + min(	results[x-1][y],
+												results[x][y-1],
+												results[x-1][y-1])
+	
+	if 1:
+		printCostMatrix(costs)
+		print ' '
+		printCostMatrix(results)
+		print ' '
+	
+	#with the costs added to the exit row and column, we can exit by the least costly path
+	result = min(results[-1] + [r[-1] for r in results])
+	return result
+	#return results[-1][-1]
--- gesture/gestureRecognizer1.py
+++ gesture/gestureRecognizer1.py
@@ -4,11 +4,16 @@ import dtw
 import distances
 
 def test():
-	lx = [[math.sin(x/10.0)] for x in range(10)]
-	ly = [[math.sin(x/10.0 + 0.3)] for x in range(10)]
+	dFunc = distances.euclideanDistance
+	#lx = [[math.sin(x/10.0)] for x in range(10)]
+	#ly = [[math.sin(x/10.0 + 0.5)] for x in range(10)]
+	#print lx, '\n'
+	#print ly, '\n'
 	
-	#print lx
-	#print ly
+	lx = [[x] for x in [0, 0, 0, 1, 2, 3, 3, 3, 2]]
+	ly = [[x] for x in [1, 2, 3, 3, 3, 2]]
+	
+	#print dFunc(lx[-1], ly[6])
 	
 	#lx = [[x] for x in [1,1,2,3,2,0]]
 	#ly = [[y] for y in [0,1,1,2,3,2,1]]
@@ -16,7 +21,7 @@ def test():
 	#print distances.euclideanDistance([1.0], [5.0])
 	
 	#dist = dtw.dtwBasic(lx, ly, distances.euclideanDistance)
-	dist = dtw.dtwAcceptPortionOfInputs(lx, ly, distances.euclideanDistance)
+	dist = dtw.dtwAcceptPortionOfInputs(lx, ly, dFunc)
 	print dist
 	
 if __name__ == "__main__":
